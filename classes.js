29725055528d7409b230a0e7e30e436f6c0ac817
Wesley Berry <wesleyberry52@gmail.com>
2020-01-27 13:43:32 -0600

    extends classes

--- classes.js
+++ classes.js
@@ -5,11 +5,28 @@ class User {
         this.password = password;
     }
 
+    static countUsers() {
+        console.log(`There are 50 users`);
+    }
+
     register() {
         console.log(`${this.username} is now registered.`);
     }
-
 }
 
-let bob = new User('bob1', 'bob@bob.com', 'abc123');
-bob.register();
+class Member extends User {
+    constructor(username, email, password, memberPackage) {
+        super(username, email, password);
+        this.memberPackage = memberPackage;
+    }
+
+    getPackage() {
+        console.log(`${this.username} is subscribed to the ${this.memberPackage} package.`)
+    }
+}
+
+// let bob = new User('bob1', 'bob@bob.com', 'abc123');
+// bob.register();
+
+let mike = new Member('mike', 'mike@email.com', 'abc123', 'Standard');
+mike.getPackage();
